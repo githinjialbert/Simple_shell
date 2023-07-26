764deff8b75e22ee8eb300da274f089d54be3d54
githinjialbert <githinjialbert001@gmail.com>
2023-07-26 02:32:15 -0700

    builtin2.c

--- builtin2.c
+++ builtin2.c
@@ -18,17 +18,17 @@ int _myhistory(info_t *info)
 */
 int unset_alias(info_t *info, char *str)
 {
-	char *q, v;
+	char *p, c;
 	int ret;
 
-	q = _strchr(str, '=');
-	if (!q)
+	p = _strchr(str, '=');
+	if (!p)
 		return (1);
-	v = *q;
-	*q = 0;
+	c = *p;
+	*p = 0;
 	ret = delete_node_at_index(&(info->alias),
 			get_node_index(info->alias, node_starts_with(info->alias, str, -1)));
-	*q = v;
+	*p = c;
 	return (ret);
 }
 
@@ -40,12 +40,12 @@ int unset_alias(info_t *info, char *str)
 */
 int set_alias(info_t *info, char *str)
 {
-	char *x;
+	char *p;
 
-	x = _strchr(str, '=');
-	if (!x)
+	p = _strchr(str, '=');
+	if (!p)
 		return (1);
-	if (!*++x)
+	if (!*++p)
 		return (unset_alias(info, str));
 	unset_alias(info, str);
 	return (add_node_end(&(info->alias), str, 0) == NULL);
@@ -58,16 +58,16 @@ int set_alias(info_t *info, char *str)
 */
 int print_alias(list_t *node)
 {
-	char *v = NULL, *x = NULL;
+	char *p = NULL, *a = NULL;
 
 	if (node)
 	{
-		v = _strchr(node->str, '=');
-		for (x = node->str; x <= v; x++)
-			_putchar(*x);
+		p = _strchr(node->str, '=');
+		for (a = node->str; a <= p; a++)
+			_putchar(*a);
 		_putchar('\'');
-		_puts(v + 1);
-		_puts("\n");
+		_puts(p + 1);
+		_puts("'\n");
 		return (0);
 	}
 	return (1);
@@ -80,8 +80,8 @@ int print_alias(list_t *node)
 */
 int _myalias(info_t *info)
 {
-	int x = 0;
-	char *v = NULL;
+	int i = 0;
+	char *p = NULL;
 	list_t *node = NULL;
 
 	if (info->argc == 1)
@@ -94,13 +94,13 @@ int _myalias(info_t *info)
 		}
 		return (0);
 	}
-	for (x = 1; info->argv[x]; x++)
+	for (i = 1; info->argv[i]; i++)
 	{
-		v = _strchr(info->argv[x], '=');
-		if (v)
-			set_alias(info, info->argv[x]);
+		p = _strchr(info->argv[i], '=');
+		if (p)
+			set_alias(info, info->argv[i]);
 		else
-			print_alias(node_starts_with(info->alias, info->argv[x], '='));
+			print_alias(node_starts_with(info->alias, info->argv[i], '='));
 	}
 	return (0);
 }

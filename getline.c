a3bb98b55b768a17e8d6c9f245e0b93255db94e2
albert <125542913+githinjialbert@users.noreply.github.com>
2023-07-26 19:54:55 +0300

    Update getline.c

--- getline.c
+++ getline.c
@@ -17,7 +17,7 @@ ssize_t input_buf(info_t *info, char **buf, size_t *len)
 		*buf = NULL;
 		signal(SIGINT, sigintHandler);
 #if USE_GETLINE
-		r = getline(buf, &len_p, stdin);
+		r = _getline(buf, &len_p, stdin);
 #else
 		r = _getline(info, buf, &len_p);
 #endif
